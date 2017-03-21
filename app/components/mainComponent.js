e0cc425eb57cd23b1fe08c98629d65e4ba73c463
Selva <Selva>
2017-03-21 11:07:39 +0530

    components

---
+++ app/components/mainComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <h3>This is Main Component</h3>
+        );
+    }
+}
+
+export default Main;
---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import Main from './components/mainComponent';
+
+ReactDOM.render((
+  <div>
+   <Main/>
+  </div>
+
+), document.getElementById('app'));
