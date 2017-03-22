147020b37ec7a8755b2e576c4052a9780aec7d0b
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-03-22 08:11:50 +0530

    server updated

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const reactDOM = require('react-dom');
 const cons = require('consolidate');
 const dust = require('dustjs-linkedin');
 const bodyParser = require('body-parser');
-
-
+const morgan = require('morgan');
+const debug = require('winston');
 
 const PORT = 5000;
 
+
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', PORT);
 
@@ -20,11 +22,16 @@ app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 
+//morgan logger
+app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-
+//index route
 app.get('/',function(req,res){
+    debug.info('Navigating to index route');
     res.render('index');
 });
+
 
 app.listen(PORT);
 console.log(`Server running on http://localhost:${PORT}`);
